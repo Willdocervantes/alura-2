d4e27ac24ffce41551c4943bea2f5f4b6db18d56
Willdocervantes <winston.principal@gmail.com>
2024-10-08 08:47:53 -0300

    Add files via upload

---
+++ FLASHCARD/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'FUTEBOL',
+    'POR QUE O PALMEIRAS NÃO TEM MUNDIAL?',
+    'PORQUE SÃO HORRIVEIS'
+)
+
+criaCartao(
+    'Geografia',
+    'QUAL A CAPITAL DO BRASIL?',
+    'DISTRITO FEDERAL'
+)
+
+criaCartao(
+    'ATUALIDADES',
+    'QUANTOS CHIFRES O JOÃO TEM?',
+    'TODOS'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
